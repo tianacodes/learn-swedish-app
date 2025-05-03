b35cea4e2da91ba7c0d436a1710d0602f1d040da
Tetiana Skazhenyk <tetiana.skazhenyk@volvocars.com>
2025-05-03 23:34:40 +0200

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,60 @@
-import { WordCarousel } from "@/components/word-carousel";
 import { db } from "@/db";
 
-export default async function Home() {
-  const slug = 'familjen'
-  const wordCardsByCategory = await db.category.findUnique({
-    where: { slug },
-    include: {
-      cards: true, // Fetch related cards
-    },
-  });
-  const wordCards = wordCardsByCategory?.cards;
+export default async function Page() {
+    const allCategories = (await db.category.findMany({
+        select: {
+            name: true,
+            slug: true,
+            cards: true
+        },
+    }));
 
-  if (!wordCards) {
-    return null;
-  }
+    const categoriesExcludingMistakes = allCategories.filter((category) => category.slug !== "mistakes");
 
-  return (
-    <div className="container mx-auto max-w-2xl pt-20">
-      <WordCarousel wordCards={wordCards} />
-    </div>
-  );
+    const mistakesCategoryData = allCategories.find((category) => category.slug === "mistakes")
+
+    return (
+        <div className="container w-full max-w-2xl pt-20 mx-auto flex flex-col gap-8">
+            {mistakesCategoryData?.cards.length ? (<>
+                <h1 className="text-2xl font-bold">Train Mistakes</h1>
+                <ul className="flex flex-col gap-4 w-full">
+                    <li>
+                        <a
+                            href="/mistakes"
+                            className="w-full flex flex-row justify-between p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                        >
+                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {mistakesCategoryData?.name}
+                            </h5>
+                            <h5 className="inline-flex items-center justify-center w-8 h-8 ms-2 text-lg font-bold text-blue-800 bg-blue-200 rounded-full">
+                                {mistakesCategoryData?.cards.length}
+                            </h5>
+                        </a>
+                    </li>
+                </ul>
+            </>) : null}
+
+            <h1 className="text-2xl font-bold">All Categories</h1>
+            <ul className="flex flex-col gap-4 w-full">
+                {categoriesExcludingMistakes.map(({ name, slug, cards }, index) => (
+                    <li key={index}>
+                        <a
+                            href={slug}
+                            className="w-full flex flex-row justify-between p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                        >
+                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {name}
+                            </h5>
+                            {/* <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {cards.length}
+                            </h5> */}
+                            <h5 className="inline-flex items-center justify-center w-8 h-8 ms-2 text-lg font-bold text-blue-800 bg-blue-200 rounded-full">
+                                {cards.length}
+                            </h5>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
