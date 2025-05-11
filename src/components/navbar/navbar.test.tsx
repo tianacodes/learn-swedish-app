f0dae4ad409b3f9e73d375e74e5bff6c160dbafe
Tetiana Skazhenyk <tetiana.skazhenyk@volvocars.com>
2025-05-12 00:51:46 +0200

    Add test for Navbar

---
+++ src/components/navbar/navbar.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Navbar from '@/components/navbar/navbar';
+import '@testing-library/jest-dom';
+
+jest.mock('next/link', () => {
+  // eslint-disable-next-line react/display-name, @typescript-eslint/no-explicit-any
+  return ({ children, href, ...rest }: { children: React.ReactNode; href: string;[key: string]: any }) => {
+    return <a href={href} {...rest}>{children}</a>;
+  };
+});
+
+describe('Navbar', () => {
+  beforeEach(() => {
+    render(<Navbar />);
+  })
+
+  test('renders the Logo link', () => {
+    const logoElement = screen.getByText('SWETYLO');
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test('Logo link has the correct href attribute', () => {
+    const logoLink = screen.getByText('SWETYLO');
+    expect(logoLink).toHaveAttribute('href', '/');
+  });
+})
