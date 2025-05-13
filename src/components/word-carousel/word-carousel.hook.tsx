3e53d5d67eaeb51fe9ec79717cf3a7dcf2c8eb3a
Tetiana Skazhenyk <tetiana.skazhenyk@volvocars.com>
2025-05-13 22:34:24 +0200

    WordCarousel re-structure

--- src/components/word-carousel/word-carousel.hook.tsx
+++ src/components/word-carousel/word-carousel.hook.tsx
@@ -1,14 +1,13 @@
-"use client";
 import { useCallback, useEffect, useState } from "react";
-import WordPracticeCard from "./word-practice-card";
-import { FinishPracticeScreen } from "./finish-practice-screen/finish-practice-screen";
 import { Category, Card } from "@/generated/prisma";
 
-export const WordCarousel = ({ categoryData }: {
+export type UseWordCarouselType = {
     categoryData: Category & {
         cards: Card[];
     };
-}) => {
+}
+
+export const useWordCarousel = ({ categoryData }: UseWordCarouselType) => {
     const [mistakesCount, setMistakesCount] = useState(0);
     const [index, setIndex] = useState(0);
     const [isDone, setIsDone] = useState(false);
@@ -66,38 +65,14 @@ export const WordCarousel = ({ categoryData }: {
         setMistakesCount(prev => prev + 1)
     }, [mistakesCount])
 
-    if (!currentCard) {
-        return null;
+    return {
+        currentCard,
+        isDone,
+        index,
+        handleClick,
+        handleMistake,
+        mistakesCount,
+        handleCategoryPracticeRestart,
+        isReady
     }
-
-    if (!isReady) return <div>Loading...</div>;
-
-    return (
-        <div className="flex flex-col gap-8">
-            <div className="flex flex-row justify-between">
-                <h1 className="text-2xl font-bold">Category: {categoryData.name}</h1>
-                <span className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">Mistakes: {mistakesCount}</span>
-            </div>
-            <div className="flex flex-col gap-4">
-                {!isDone ? (
-                    <div className="w-full relative mx-auto flex max-w-2xl items-center rounded-xl bg-white p-10 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-                        <div className="absolute top-5 right-5">
-                            <p className="text-gray-500 font-bold">{index + 1}/{categoryData.cards.length}</p>
-                        </div>
-                        <WordPracticeCard
-                            key={currentCard.id}
-                            categorySlug={categoryData.slug}
-                            term={currentCard.term}
-                            answer={currentCard.answer}
-                            handleClick={handleClick}
-                            handleMistake={handleMistake}
-                        />
-                    </div>
-                ) : null}
-                {isDone ? (
-                    <FinishPracticeScreen mistakes={`${mistakesCount}/${categoryData.cards.length}`} handleCategoryPracticeRestart={handleCategoryPracticeRestart} />
-                ) : null}
-            </div>
-        </div>
-    );
-};
+}
