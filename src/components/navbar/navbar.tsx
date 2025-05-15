d0a494fe28e4008b1a44128f063f199b8f86a8e4
Tetiana Skazhenyk <tetiana.skazhenyk@volvocars.com>
2025-05-15 19:21:20 +0200

    Intentionally did a bug in Navbar to see Regression test results

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="flex items-center justify-between px-4 py-3 bg-white shadow-md sticky top-0 z-50">
             <Link href="/" className="text-xl font-bold bg-gradient-to-br from-blue-600 to-yellow-400 bg-clip-text text-transparent">
-                SWETYLO
+                SWEDISH
             </Link>
             <div className="w-10" />
         </nav>
